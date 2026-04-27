778d5b665507943670e57f8b89aca854c314f81b
acallender1 <93797128+acallender1@users.noreply.github.com>
2026-04-27 18:51:16 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'muva-studio-v5';
+const CACHE = 'muva-studio-v6';
 const ASSETS = [
-    '/muva-studio/',
-    '/muva-studio/index.html',
-    '/muva-studio/icon192.png',
-    '/muva-studio/icon512.png'
+    '/',
+    '/index.html',
+    '/icon192.png',
+    '/icon512.png'
 ];
 
 self.addEventListener('install', e => {
@@ -34,6 +34,6 @@ self.addEventListener('fetch', e => {
                 caches.open(CACHE).then(cache => cache.put(e.request, clone));
                 return res;
             });
-        }).catch(() => caches.match('/muva-studio/index.html'))
+        }).catch(() => caches.match('/index.html'))
     );
 });
